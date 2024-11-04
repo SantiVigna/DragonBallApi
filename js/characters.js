96fa2be266aab53187c5ada8ca5f215538e814a9
SantiVigna <ynotvigna@gmail.com>
2024-11-04 16:05:28 +0100

    create structure and design for pages

---
+++ js/characters.js
@@ -0,0 +1,45 @@
+const requestURL = "https://dragonball-api.com/api/characters?page=1&limit=58"
+
+async function fetchCharacterJson(){
+    try{
+      const response = await fetch(requestURL);
+      if (!response.ok) {
+          throw new Error(`Error en la peticion ${response.status}`);
+      }
+      return await response.json();
+  }
+  catch (error){
+      console.error(`Error al obtener los personajes de la API : `,error);
+      return null;
+  }
+}
+
+function createCharacterCard ({id, name, ki, maxKi, race, gender, description, image}){
+  return `<div class="card">
+            <img class="card-img-top" src="${image}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${race} - ${gender}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Ki: ${ki}</li>
+                <li class="list-group-item">Maximum Ki: ${maxKi}</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+  `;
+}
+
+async function displayCharacters() {
+  const characterSection = document.getElementById('charactersection');
+  const charactersData = await fetchCharacterJson();
+
+  if (charactersData && charactersData.items){
+      const characterCards = charactersData.items.map(createCharacterCard).join('');
+      characterSection.innerHTML = characterCards;
+  }
+  else{
+    characterSection.innerHTML = `<p>No se ha podido cargar el Json de los personajes</p>`
+  }
+}
+displayCharacters();
---
+++ js/planets.js
@@ -0,0 +1,43 @@
+const requestURL = "https://dragonball-api.com/api/planets?limit=58"
+
+async function fetchPlanetsJson(){
+    try{
+      const response = await fetch(requestURL);
+      if (!response.ok) {
+          throw new Error(`Error en la peticion ${response.status}`);
+      }
+      return await response.json();
+  }
+  catch (error){
+      console.error(`Error al obtener los planetas de la API : `,error);
+      return null;
+  }
+}
+
+function createPlanetCard ({id, name, description, image}){
+  return `<div class="card" style="width: 18rem;">
+          <img src="${image}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h4 class="card-title">${name}</h4>
+              <p class="card-text">${description}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item"></li>
+          </ul>
+      </div>
+  `;
+}
+
+async function displayPlanets() {
+  const planetSection = document.getElementById('planetsection');
+  const planetData = await fetchPlanetsJson();
+
+  if (planetData && planetData.items){
+      const planetCards = planetData.items.map(createPlanetCard).join('');
+      planetSection.innerHTML = planetCards;
+  }
+  else{
+    planetSection.innerHTML = `<p>No se ha podido cargar el Json de los planetas</p>`
+  }
+}
+displayPlanets();
